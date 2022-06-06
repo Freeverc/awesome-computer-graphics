fc4be7e1ca176aefed32e6c9edffd2193e7c9a94
freeverc <freeverchen@126.com>
2022-06-06 17:50:35 +0800

    comments

--- ModelMoving/obj-viewer.cpp
+++ ModelMoving/obj-viewer.cpp
@@ -13,7 +13,7 @@
 #include <fstream>
 #include <regex>
 
-
+// 定义一些基本的数据结构 
 typedef struct OBJ_COLOR {
     GLfloat red, green, blue;
     OBJ_COLOR() : red(1.0), green(1.0), blue(1.0) {}
@@ -31,6 +31,7 @@ typedef struct face_quad {
     unsigned long v1, v2, v3, v4;
 } face_quad;
 
+// 要显示的模型
 struct ObjModel {
     ObjModel(double x_, double y_, double z_) : position(x_, y_, z_){};
     std::vector<vertex> vertices;
@@ -40,9 +41,11 @@ struct ObjModel {
     ObjColor color;
 };
 
-bool is_quad;
+
+bool is_quad; // 是矩形的模型还是三角形
 bool render_mode; // true = solid body, false = wireframe
 
+// 一些参数
 const float ZOOM_SPEED = 0.2f;
 const float ROTATE_SPEED = 0.2f;
 const float MOVE_SPEED = 0.5f;
@@ -56,12 +59,14 @@ struct camera {
 
 std::vector<ObjModel> models;
 
+// 绘制三角形
 void drawTriangle(vertex v1, vertex v2, vertex v3) {
 	glVertex3f(v1.x, v1.y, v1.z);
 	glVertex3f(v2.x, v2.y, v2.z);
 	glVertex3f(v3.x, v3.y, v3.z);
 }
 
+// 绘制场景, 场景包含5个平面, 每个平面的颜色具有渐变效果
 void drawRoom() {
 	glBegin(GL_TRIANGLES);
 	for (int x = 0; x < 500; x++) {
@@ -147,6 +152,7 @@ void drawRoom() {
 	glPopMatrix();
 }
 
+// 初始化，设置光线等，这里不需要场景太复杂,不需要复杂的光线，就注掉了
 void init() {
     // glShadeModel(GL_SMOOTH);
     // glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -176,6 +182,7 @@ void init() {
 
 }
 
+// 计算平面的法向量，用于渲染网格
 void calculate_normal(ObjModel& model, face_triangle f, GLdouble *normal) {
     // x
     normal[0] = (model.vertices[f.v2 - 1].y - model.vertices[f.v1 - 1].y) * (model.vertices[f.v3 - 1].z - model.vertices[f.v1 - 1].z)
@@ -188,14 +195,18 @@ void calculate_normal(ObjModel& model, face_triangle f, GLdouble *normal) {
                 - (model.vertices[f.v3 - 1].x - model.vertices[f.v1 - 1].x) * (model.vertices[f.v2 - 1].y - model.vertices[f.v1 - 1].y);
 }
 
+// 绘制模型
 void draw_obj() {
     if (render_mode){
+        // 绘制填充图形
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    }
-    else {
+    } else {
+        // 不填充图形
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     }
+
     if (is_quad) {
+        // 绘制矩形模型
         for(ObjModel model:models) {
             for (face_quad f : model.faces_quads) {
                 // Calculate normal by calculating two tri normals & averaging
@@ -222,6 +233,7 @@ void draw_obj() {
             }
         }
     } else {
+        // 绘制三角模型
         for(ObjModel model : models) {
             for (face_triangle f : model.faces_triangles) {
                 GLdouble normal[3];
@@ -239,6 +251,7 @@ void draw_obj() {
     glFlush();
 }
 
+// 改变窗口大小
 void reshape(int w, int h) {
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
@@ -252,8 +265,10 @@ void reshape(int w, int h) {
     glLoadIdentity();
 }
 
+// 方向键的处理，用于调整相机视角
 void arrow_keys(int key, int x, int y) {
     switch (key) {
+            // 改变相机距离
         case GLUT_KEY_UP: {
             DISTANCE -= ZOOM_SPEED;
             break;
@@ -262,6 +277,7 @@ void arrow_keys(int key, int x, int y) {
             DISTANCE += ZOOM_SPEED;
             break;
         }
+            // 改变相机角度
         case GLUT_KEY_LEFT: {
             camera.theta -= ROTATE_SPEED;
             break;
@@ -274,6 +290,7 @@ void arrow_keys(int key, int x, int y) {
     }
 }
 
+// 普通键的处理，用于移动物体、切换要移动的物体等。
 void keyboard(unsigned char key, int x, int y) {
     switch (key) {
         case 27:
@@ -281,22 +298,26 @@ void keyboard(unsigned char key, int x, int y) {
         case 'm':
             render_mode = !render_mode;
             break;
+            // 改变物体
         case 'c':
             move_id++;
             move_id %= 3;
             break;
+            // 水平移动
         case 'w':
             models[move_id].position.x += MOVE_SPEED;
             break;
         case 's':
             models[move_id].position.x -= MOVE_SPEED;
             break;
+            // 水平移动
         case 'a':
             models[move_id].position.z += MOVE_SPEED;
             break;
         case 'd':
             models[move_id].position.z -= MOVE_SPEED;
             break;
+            // 上下移动
         case 'q':
             models[move_id].position.y += MOVE_SPEED;
             break;
@@ -308,7 +329,9 @@ void keyboard(unsigned char key, int x, int y) {
     }
 }
 
+// 显示界面，调用上面的一些函数
 void display() {
+    // 先初始化相机
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
@@ -317,12 +340,14 @@ void display() {
     camera.z =        DISTANCE * cos(camera.theta);
 
     gluLookAt(camera.x, camera.y, camera.z, 0, 2.0f, 0, 0.0f, 1.0f, 0.0f);
+    // 绘制场景和物体
     drawRoom();
     draw_obj();
     glutSwapBuffers();
     glutPostRedisplay();
 }
 
+// 读取obj文件的每一行
 void obj_parse(char *file_line , ObjModel& model) {
 
     if (file_line[0] == '\0') {
@@ -369,6 +394,7 @@ void obj_parse(char *file_line , ObjModel& model) {
     }
 }
 
+// 读取obj文件，转化成我们定义的模型
 void open_obj(std::string& filename, ObjModel& model) {
 
     //// Argument parsing ////
@@ -407,6 +433,8 @@ int main(int argc, char *argv[]) {
 w a s d q e : Move objects\n \
 c : change object to move \n \
 t : switch between rendering faces and not\n" << std::endl;
+
+    // 读取三个文件
     std::string filename_0 = "./models/bunny.obj";
     std::string filename_1 = "./models/sandal_mini.obj";
     std::string filename_2 = "./models/cessna_mini.obj";
@@ -422,6 +450,7 @@ t : switch between rendering faces and not\n" << std::endl;
     // initialize rendering with solid body
     render_mode = true;
 
+    // 初始化
     std::cout << "Running..." << std::endl;
     int window;
     glutInit(&argc, argv);
@@ -430,9 +459,11 @@ t : switch between rendering faces and not\n" << std::endl;
     glutInitWindowPosition(0, 0);
     window = glutCreateWindow("Justin Tennant OBJ Visualizer");
     init();
+    // 设置各种回调函数
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutSpecialFunc(arrow_keys);
     glutKeyboardFunc(keyboard);
+    // 程序真正跑起来
     glutMainLoop();
 }
